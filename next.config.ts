4aa51107e2010a77a7d99fcaf3a062cfe4e7c181
Вячеслав <tfdf63@yandex.ru>
2025-03-22 01:33:14 +0400

    добавил Merch на 15 изображений продукции с рандомным отображением. upd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	trailingSlash: true,
 	skipTrailingSlashRedirect: true,
+	output: 'export',
+	distDir: 'out',
 }
 
 export default nextConfig
