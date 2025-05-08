54588e6cde545097531e72fbac1c8963806d8c71
Вячеслав <tfdf63@yandex.ru>
2025-05-08 18:46:20 +0400

    upd widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import FanCard from '@/components/FanCard/FanCard'
 import Merch from '@/components/Merch/Merch'
 import Stadium from '@/components/Stadium/Stadium'
 import Skybox from '@/components/Skybox/Skybox'
+import YandexTicket from '@/components/YandexTicket/YandexTicket'
 // import VIP from '@/components/VIP/VIP'
 const MatchesPage = () => {
 	return (
@@ -34,6 +35,7 @@ const MatchesPage = () => {
 			/>
 			<Skybox />
 			<Stadium />
+			<YandexTicket />
 			{/* <VIP /> */}
 		</>
 	)
