0191b25fb8afc5053fa9b7c3e5d128cb6273c459
Вячеслав <tfdf63@yandex.ru>
2025-09-14 19:00:42 +0400

    add cup

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
 import styles from './Main.module.scss'
 import CardMatch from '../CardMatch/CardMatch'
 // import Timer from '../Timer/Timer'
