529e55b8d31621c82c54528a5132a9af56b2cfaf
Вячеслав <tfdf63@yandex.ru>
2025-11-12 11:13:55 +0400

    upd 11.11

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
 import styles from './Main.module.scss'
 import CardMatch from '../CardMatch/CardMatch'
 // import Timer from '../Timer/Timer'
