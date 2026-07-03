813f928a352e06bd69ca8ce4104240badb7b11fb
Вячеслав <tfdf63@yandex.ru>
2026-07-03 13:38:22 +0400

    add season first round

--- src/features/home/mainHeroConfig.ts
+++ src/features/home/mainHeroConfig.ts
@@ -4,10 +4,10 @@ export type MainHeroCardMode = 'match' | 'seasonTickets'
 export const MAIN_HERO_CARD_MODE: MainHeroCardMode = 'seasonTickets'
 
 export const SEASON_TICKETS_BUY_URL =
-	'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-2130@season-65811185?clientKey=d721bb72-e7ce-4a03-8775-67aea527feb0&regionId=51'
+	'https://widget.afisha.yandex.ru/w/venues/79807?clientKey=d721bb72-e7ce-4a03-8775-67aea527feb0&regionId=51'
 
 export const SEASON_TICKETS_VIP_URL =
-	'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-7858@season-65811185?clientKey=0046af24-2980-419c-bf99-c4d864c693e3&regionId=51'
+	'https://widget.afisha.yandex.ru/w/venues/85000?clientKey=0046af24-2980-419c-bf99-c4d864c693e3&regionId=51'
 
 export const SEASON_TICKETS_FAMILY_URL =
-	'https://widget.afisha.yandex.ru/w/sessions/ticketsteam-7854@season-65811185?clientKey=cbdbe66d-8aa4-40e2-a435-894fe798cf5e&regionId=51'
+	'https://widget.afisha.yandex.ru/w/venues/85002?clientKey=cbdbe66d-8aa4-40e2-a435-894fe798cf5e&regionId=51'
